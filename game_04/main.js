f86c24e06cf8a0f58fbd8adee1879c07d92eaa84
Gleb Vasilchenko <glebas@tut.by>
2017-10-19 14:21:38 +0300

    Угодайка, удалили комментарии

--- game_04/main.js
+++ game_04/main.js
@@ -10,10 +10,6 @@ var matrix=new Array();
 var $$ = document.querySelector.bind(document);
 var $$All = document.querySelectorAll.bind(document);
 
-console.log(34);
-//buttonSubmit.addEventListener("onclick",startGame());
-console.log(44);
-
 function Picture(pictureNumber, urlPicture) {  //это ячейка таблицы.
     var id=pictureNumber;
     var url=urlPicture;
@@ -65,7 +61,6 @@ function getPicturesArray(size){
         "technics",
         "transport"
       ];
-      console.log(2);
       var arr=[];
       // Создаем объект { категория: [10 картинок] }
       var images = categories.reduce((hash, category) => {
@@ -85,7 +80,6 @@ function getPicturesArray(size){
         var currentPicture=new Picture(result.size,arr[getRandomValue(arr.length)]);
         result.add(currentPicture);
       }
-      console.log("result="+result);
       return result;
 }
 
@@ -93,7 +87,6 @@ function getRandomValue(size){
     var randomValue=Math.random();
     var value=randomValue*size;
     value=Math.floor(value);
-    console.log("randomValue="+randomValue+", value="+value);
 
     return value;
 }
@@ -102,7 +95,6 @@ function startGame() {
     gameOver=false;
     openedCards=0;
     matrix=[];
-    console.log(1);
     firstDemention=document.getElementById("firstDemention").value;
     secondDemention=document.getElementById("secondDemention").value;
     var setPictures=getPicturesArray(firstDemention*secondDemention/2);
@@ -115,26 +107,21 @@ function startGame() {
     var tableHTML= `<table class="images" id="placeImages">`;
 
     for (var i=0;i<firstDemention;i++){  //распределяем по матрице и выводим на экран
-        console.log("i="+i);
         tableHTML+= `<tr>`;
         matrix[i]=new Array();
         for (var j=0;j<secondDemention;j++){
-            console.log("j="+j);
-           
+         
             var currentNumber=getRandomValue(arr.length);
             var currentPicture=arr[currentNumber];
             matrix[i][j]=currentPicture;
             arr.splice(currentNumber,1);
-            console.log("arr.length="+arr.length+", currentPicture.id="+currentPicture.getId());
             tableHTML+= getCardImageHTML(i,j,true); //изначально инициализируем рубашкой вверх   
          }
          tableHTML+= `</tr>`;
     }
     tableHTML+= `</table>`;
     $$(".place").innerHTML=tableHTML;
-    //$$(".place").addEventListener("onclick",onCardClick);
     document.getElementById("placeImages").onclick=function (event){
-        console.log(9);
         if (event.target.localName!=="img" || gameOver){
             return;
         }
@@ -146,7 +133,6 @@ function startGame() {
             matrix[currentI][currentJ].toOpen(true);
             document.getElementById("cell"+currentI+currentJ).innerHTML= getCardImageHTML(currentI,currentJ,false);
         }        
-        console.log(10);
         if (openedCards===3){ //закрываем все открытые, открываем только текущаю
             for (var i=0;i<firstDemention;i++){  //распределяем по матрице и выводим на экран
                 for (var j=0;j<secondDemention;j++){
@@ -163,7 +149,7 @@ function startGame() {
         }else if (openedCards===2){
             var currentCell= matrix[currentI][currentJ];
               
-            for (var i=0;i<firstDemention;i++){  //распределяем по матрице и выводим на экран
+            for (var i=0;i<firstDemention;i++){  
                 for (var j=0;j<secondDemention;j++){
                     if (i!==currentI || j!==currentJ){
                         if (matrix[i][j].isOpened() && matrix[i][j].getId()===currentCell.getId()){ //Типа, нашли пару
@@ -173,7 +159,6 @@ function startGame() {
                             document.getElementById("cell"+currentI+currentJ).innerHTML= '<td>-1<div class="empty" style="width:80px;height:100px;"></td>';
                             currentCell.setPicture(undefined);
                             openedCards=0;
-                            console.log("Нашли пару");
             
                             break;
                         }
@@ -184,10 +169,6 @@ function startGame() {
         }
         checkForGameOver();
     }
-    
-
-console.log(7);
-
 }
 
 function getCardImageHTML(i,j,backSide){
@@ -211,29 +192,10 @@ function checkForGameOver(){
         }
         if (!gameOver){
             break;
-            console.log(12);
         }
     }        
     if (gameOver){
-        matrix=[];
-        console.log("Game over 2");
         alert("Game over");
     }
-    console.log(14);
-    
+ 
 }
-
-
-// Создание таблицы строки - категории, в ячейках - картинки
-/*
-$$(".place").innerHTML =${Array(categories.length)
-  .fill("<tr></tr>")
-  .join("")}</table>`;
-categories.forEach(
-  (category, index) =>
-    ($$All(`.images tr`)[index].innerHTML = images[category]
-      .map(img => `<td><img src="${img}" /></td>`)
-      .join(""))
-);
-
-}*/
